42415f8b8f64396042752175606591c43e5d5833
waghshivam322@gmail.com <waghshivam322@gmail.com>
2025-08-04 00:00:30 +0530

    feat: Add palindrome checker and update factorial program

--- FactorialOfN.java
+++ FactorialOfN.java
@@ -3,9 +3,9 @@ public class FactorialOfN {
     public static int factorial(int n)
     {
        int res=1;
-       for(int i=2;i<n;i++)
+       for(int i=2;i<=n;i++)
        {
-        res*=i;//i.e res=res+i;
+        res*=i;//i.e res=res+i; add factorial such as 5!=5*4*3*2*1=120
        }
        return res;
     }
